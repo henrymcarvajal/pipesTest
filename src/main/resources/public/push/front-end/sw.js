a7abaf5a86312f3fa1f9770076ab904a6fbfd30f
Henry Mauricio Carvajal <hmcarvajal@ayax.co>
2017-07-17 22:51:09 -0500

    Push notifications

--- src/main/resources/public/push/front-end/sw.js
+++ src/main/resources/public/push/front-end/sw.js
@@ -46,9 +46,9 @@ self.addEventListener('push', function(event) {
   console.log('[Service Worker] Push Received.');
   console.log('[Service Worker] Push had this data: "${event.data.text()}"');
 
-  const title = 'Push Codelab';
+  const title = 'Push Message';
   const options = {
-    body: '${event.data.text()}',
+    body: event.data.text(),
     icon: 'images/icon.png',
     badge: 'images/badge.png'
   };
