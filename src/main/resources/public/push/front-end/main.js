e96a21e7965bb4eff1a2b4e6f9eab1ccd2775722
Henry Mauricio Carvajal <hmcarvajal@ayax.co>
2017-07-19 00:08:39 -0500

    push test

--- src/main/resources/public/push/front-end/main.js
+++ src/main/resources/public/push/front-end/main.js
@@ -21,7 +21,7 @@
 
 'use strict';
 
-const applicationServerPublicKey = '<Your Public Key>';
+const applicationServerPublicKey = 'BMKhzaOd6Ry4ELWfXVuacN4yjEG-9ke6vJYS_MczNW6oUVIYwgINt2hqFuRh-KTgENOd1Y0mY7kOR7CK_u_vxIY';
 
 const pushButton = document.querySelector('.js-push-btn');
 
@@ -51,6 +51,7 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
     console.log('Service Worker is registered', swReg);
 
     swRegistration = swReg;
+      initialiseUI();
   })
   .catch(function(error) {
     console.error('Service Worker Error', error);
@@ -58,4 +59,30 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
 } else {
   console.warn('Push messaging is not supported');
   pushButton.textContent = 'Push Not Supported';
+}
+
+function initialiseUI() {
+  // Set the initial subscription value
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    isSubscribed = !(subscription === null);
+
+    if (isSubscribed) {
+      console.log('User IS subscribed.');
+    } else {
+      console.log('User is NOT subscribed.');
+    }
+
+    updateBtn();
+  });
+}
+
+function updateBtn() {
+  if (isSubscribed) {
+    pushButton.textContent = 'Disable Push Messaging';
+  } else {
+    pushButton.textContent = 'Enable Push Messaging';
+  }
+
+  pushButton.disabled = false;
 }
